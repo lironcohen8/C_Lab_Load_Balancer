261af8f24813c1be2eae360d212154baab447c01
Liron <lironcohen3@mail.tau.ac.il>
2023-05-12 22:38:42 +0300

    fixes for gcc

--- ex3_lb.c
+++ ex3_lb.c
@@ -5,7 +5,6 @@
 
 #define SERVER_PORT_FILE_NAME "server_port"
 #define HTTP_PORT_FILE_NAME "http_port"
-#define INVALID_SOCKET -1
 #define NUMBER_OF_CLIENTS 1
 #define NUMBER_OF_SERVERS 3
 #define BUFFER_SIZE 1024
@@ -19,17 +18,17 @@ int main() {
     write_socket_port_to_file(http_socket, HTTP_PORT_FILE_NAME);
 
     int server_sockets[NUMBER_OF_SERVERS];
-    wait_for_servers_connections(&server_sockets, NUMBER_OF_SERVERS);
+    wait_for_servers_connections(server_sockets, server_socket, NUMBER_OF_SERVERS);
 
     int next_server_index = 0;
     while (true) {
         int current_server_socket = server_sockets[next_server_index];
         int client_socket = accept_connection_on_socket(http_socket);
-        char* buffer = buffer[BUFFER_SIZE];
-        receive_data_from_socket(client_socket, &buffer, BUFFER_SIZE);
-        send_data_to_socket(current_server_socket, &buffer, BUFFER_SIZE);
-        receive_data_from_socket(current_server_socket, &buffer, BUFFER_SIZE);
-        send_data_to_socket(client_socket, &buffer, BUFFER_SIZE);
+        char buffer[BUFFER_SIZE];
+        receive_data_from_socket(client_socket, buffer, BUFFER_SIZE);
+        send_data_to_socket(current_server_socket, buffer, BUFFER_SIZE);
+        receive_data_from_socket(current_server_socket, buffer, BUFFER_SIZE);
+        send_data_to_socket(client_socket, buffer, BUFFER_SIZE);
         next_server_index = (next_server_index + 1) % NUMBER_OF_SERVERS;
     }
 
--- socket_handler.c
+++ socket_handler.c
@@ -1,18 +1,24 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#define INVALID_SOCKET -1
-#define BUFFER_SIZE 1024
+#define SOCKET_ERROR -1
+#define LOCAL_HOST "127.0.0.1"
+#define MIN_PORT_NUMBER 1024
+#define MAX_PORT_NUMBER 64000
 
 /* private functions */
-void bind_socket_to_a_free_port(socket, sock_addr) {
+void bind_socket_to_a_free_port(int socket, struct sockaddr_in sock_addr) {
     while (true) {
-        int port = rand() % (64000 - 1024 + 1) + 1024;
+        int port = rand() % (MAX_PORT_NUMBER - MIN_PORT_NUMBER + 1) + MIN_PORT_NUMBER;
         sock_addr.sin_port = htons(port);
-        setsockopt(socket, SOL_SOCKET, SO_REUSEADDR, &1, sizeof(int)); // TODO: check that it won't ruin the "busy" port check
-        if (bind(socket, (struct sockaddr_in *) &sock_addr, sizeof(asock_addrddr)) == 0) {
+        int enable = 1;
+        socklen_t addr_size = sizeof(sock_addr);
+        setsockopt(socket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)); // TODO: check that it won't ruin the "busy" port check
+        if (bind(socket, (const struct sockaddr *) &sock_addr, addr_size) == 0) {
             break;
         }
     }
@@ -20,74 +26,72 @@ void bind_socket_to_a_free_port(socket, sock_addr) {
 
 int get_socket_port(int socket) {
     struct sockaddr_in sock_addr;
-    int sock_addr_len = sizeof(sock_addr);
-    getsockname(socket, (struct sockaddr *) &sock_addr, &sock_addr_len);
+    socklen_t addr_size = sizeof(sock_addr);
+    getsockname(socket, (struct sockaddr *) &sock_addr, &addr_size);
     return ntohs(sock_addr.sin_port);
 }
 
 /* public functions */
 int create_socket_connection(int backlog) {
-    int socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (socket == INVALID_SOCKET) {
-        printf("Error at socket creation: %ld\n", WSAGetLastError());
-        WSACleanup();
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == SOCKET_ERROR) {
+        printf("Error at socket creation\n");
         exit(1);
     }
 
     struct sockaddr_in sock_addr;
     sock_addr.sin_family = AF_INET;
-    sock_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    bind_socket_to_a_free_port(socket, sock_addr);
-    int ret_val = listen(socket, backlog);
+    sock_addr.sin_addr.s_addr = inet_addr(LOCAL_HOST);
+    bind_socket_to_a_free_port(sock, sock_addr);
+    int ret_val = listen(sock, backlog);
     if (ret_val != 0) {
-        printf("Error at socket listening: %ld\n", WSAGetLastError());
-        WSACleanup();
+        printf("Error at socket listening\n");
         exit(1);
     }
-    return socket;
+    return sock;
 }
 
-int write_socket_port_to_file(int socket, char* file_name) {
+void write_socket_port_to_file(int socket, char* file_name) {
     int port = get_socket_port(socket);
     FILE *file = fopen(file_name, "w");
     if (file == NULL) {
-        printf("Error opening file\n");
+        printf("Error opening file %s\n", file_name);
         exit(1);
     }
     fprintf(file, "%d", port);
     fclose(file);
 }
 
-void wait_for_servers_connections(int* server_sockets, int number_of_servers) {
+int accept_connection_on_socket(int socket) { 
+    struct sockaddr client_sock_addr;
+    socklen_t addr_size = sizeof(client_sock_addr);
+    int client_socket = accept(socket, &client_sock_addr , &addr_size); // TODO: name may be confusing
+    if (client_socket == SOCKET_ERROR) {
+        printf("Error at socket accepting\n");
+        exit(1);
+    }
+    return client_socket;
+}
+
+
+void wait_for_servers_connections(int* server_sockets, int server_socket, int number_of_servers) {
     for (int i = 0; i < number_of_servers; i++) {
         server_sockets[i] = accept_connection_on_socket(server_socket);
     }
 }
 
-int accept_connection_on_socket(int socket) {
-    int client_socket = accept(socket); // TODO: name may be confusing
-    if (client_socket == INVALID_SOCKET) {
-        printf("Error at socket accepting: %ld\n", WSAGetLastError());
-        WSACleanup();
-        exit(1);
-    }
-    return socket;
-}
-
 void receive_data_from_socket(int socket, char* buffer, int buffer_size) {
     int bytes_received = recv(socket, buffer, buffer_size, 0);
-    if (bytes_received == SOCKET_ERROR) { # TODO: need to realloc buffer until \r\n\r\n
-        printf("Error at socket receiving: %ld\n", WSAGetLastError());
-        WSACleanup();
+    if (bytes_received == SOCKET_ERROR) { // TODO: need to realloc buffer until \r\n\r\n
+        printf("Error at socket receiving\n");
         exit(1);
     }
 }
 
 void send_data_to_socket(int socket, char* buffer, int buffer_size) {
     int bytes_sent = send(socket, buffer, buffer_size, 0);
-    if (bytes_sent == SOCKET_ERROR) { # TODO: need to realloc buffer until \r\n\r\n
-        printf("Error at socket sending: %ld\n", WSAGetLastError());
-        WSACleanup();
+    if (bytes_sent == SOCKET_ERROR) { // TODO: need to realloc buffer until \r\n\r\n
+        printf("Error at socket sending\n");
         exit(1);
     }
 }
