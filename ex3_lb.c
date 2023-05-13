5b0d1bcbd116ea07f099c0f77a93a81f689736b8
Liron <lironcohen3@mail.tau.ac.il>
2023-05-13 18:06:37 +0300

    Update ex3_lb.c

--- ex3_lb.c
+++ ex3_lb.c
@@ -12,17 +12,17 @@
 
 int main() {
     srand(time(NULL));
-    int server_socket = create_socket_connection(NUMBER_OF_SERVERS); // TODO: we need to think of a better naming since we have the lb->server socket and the server->lb socket etc
-    write_socket_port_to_file(server_socket, SERVER_PORT_FILE_NAME);
+    int listening_server_socket = create_socket_connection(NUMBER_OF_SERVERS);
+    write_socket_port_to_file(listening_server_socket, SERVER_PORT_FILE_NAME);
     int http_socket = create_socket_connection(NUMBER_OF_CLIENTS);
     write_socket_port_to_file(http_socket, HTTP_PORT_FILE_NAME);
 
-    int server_sockets[NUMBER_OF_SERVERS];
-    wait_for_servers_connections(server_sockets, server_socket, NUMBER_OF_SERVERS);
+    int active_server_sockets[NUMBER_OF_SERVERS];
+    wait_for_servers_connections(active_server_sockets, listening_server_socket, NUMBER_OF_SERVERS);
 
     int next_server_index = 0;
     while (true) {
-        int current_server_socket = server_sockets[next_server_index];
+        int current_server_socket = active_server_sockets[next_server_index];
         int client_socket = accept_connection_on_socket(http_socket);
         char buffer[BUFFER_SIZE];
         receive_data_from_socket(client_socket, buffer, BUFFER_SIZE);
