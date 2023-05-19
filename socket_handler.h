5443b040ec5e010f8890c679a1b77f478ed855e1
RonFed <ronfederman@mail.tau.ac.il>
2023-05-19 15:20:16 +0300

    complete integration to pass the test

--- socket_handler.h
+++ socket_handler.h
@@ -5,7 +5,7 @@ int create_socket_connection(int backlog);
 void write_socket_port_to_file(int socket, char* file_name);
 void wait_for_servers_connections(int* server_sockets, int server_socket, int number_of_servers);
 int accept_connection_on_socket(int socket);
-void receive_data_from_socket(int socket, char* buffer, int buffer_size);
-void send_data_to_socket(int socket, char* buffer, int buffer_size);
+int receive_data_from_socket(int socket, char** buffer, int buffer_size, unsigned int sub_string_appearnces);
+void send_data_to_socket(int socket, char* buffer, int bytes_to_send);
 
 #endif
